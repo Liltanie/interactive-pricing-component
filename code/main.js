d92c5b365ca524ff0494e277f55698dbea926634
Candela Bramucci <candelabramucci05@gmail.com>
2021-09-13 10:56:41 -0300

    slider styles

--- code/main.js
+++ code/main.js
@@ -4,6 +4,7 @@ const price = document.querySelector('.price')
 const views = document.querySelector('.views')
 const slider = document.getElementById('slider')
 const switchBilling = document.getElementById('switch')
+const progressBar = document.querySelector('.progress-bar__fill')
 
 const prices = [
 	{ pageviews: '10K', pricePerMonth: 8 },
@@ -19,6 +20,7 @@ addEventListener('DOMContentLoaded', e => {
 
 slider.addEventListener('input', e => {
 	setPrice(slider.value)
+	changeProgressBar(slider.value)
 })
 
 switchBilling.addEventListener('input', e => {
@@ -30,6 +32,14 @@ const setPrice = value => {
 	if (switchBilling.checked) billing -= (billing * 25) / 100
 	price.textContent = billing
 	views.textContent = prices[value].pageviews
+}
+
+const changeProgressBar = value => {
+	let progress = 0
+	for (let i = 0; i < value; i++) {
+		progress += 25
+	}
+	progressBar.style.width = `${progress}%`
 }
 
 //save in local storage
